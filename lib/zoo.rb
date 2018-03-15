00fa03454008fdc63f46b2c46c0af0fbf3c2e8a9
Timothy Hollis <timothyhollis@Timothys-MacBook-Pro.local>
2018-03-15 12:21:41 -0400

    more zoo methods

--- lib/zoo.rb
+++ lib/zoo.rb
@@ -5,8 +5,8 @@ class Zoo
     @name = name
     @season_opening_date = season_opening_date
     @season_closing_date = season_closing_date
-    @cages = []
     @employees = []
+    @cages = []
     10.times do
       @cages << Cage.new
     end
@@ -15,4 +15,20 @@ class Zoo
   def add_employee(person)
     @employees << person
   end
+
+  def open?(date)
+    (@season_opening_date < date) && (@season_closing_date > date)
+  end
+
+  def add_animal(new_animal)
+    @cages.each do |cage|
+      if cage.empty?
+        return cage.animal = new_animal
+      end
+    end
+
+    return "Your zoo is already at capacity!"
+  end
+
+
 end
